7a749aba7a733318730644e82f8e7155ab140083
Aresh Saharkhiz <saharkiz@yahoo.com>
2020-03-22 01:42:47 +0800

    Add files via upload

---
+++ adminator/config.php
@@ -0,0 +1,37 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://littlethingsme.com/adminator/');
+define('HTTP_CATALOG', 'http://littlethingsme.com/');
+
+// HTTPS
+define('HTTPS_SERVER', 'https://littlethingsme.com/adminator/');
+define('HTTPS_CATALOG', 'https://littlethingsme.com/');
+
+// DIR
+define('DIR_APPLICATION', '/home/littlethingsme/public_html/adminator/');
+define('DIR_SYSTEM', '/home/littlethingsme/public_html/system/');
+define('DIR_IMAGE', '/home/littlethingsme/public_html/image/');
+//define('DIR_STORAGE', DIR_SYSTEM . 'storage/');
+define('DIR_STORAGE', '/home/littlethingsme/storage/');
+define('DIR_CATALOG', '/home/littlethingsme/public_html/catalog/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', '**************');
+define('DB_PASSWORD', '**************');
+define('DB_DATABASE', '**************');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
+
+// OpenCart API
+//define('OPENCART_SERVER', 'https://portal.altannan.com');
